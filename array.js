dae2c77aa9961a80712d542d99bd715360b8ffc0
Subash80729 <108263629+Subash80729@users.noreply.github.com>
2022-07-29 23:52:23 +0530

    uploaded

---
+++ array.js
@@ -0,0 +1,37 @@
+var arr =["good","bad","loyal","outstanding"]
+var newarr =arr. concat(["excellence","better"])
+
+// console.log(newarr)
+ 
+newarr.push("outcome")
+
+// console.log(newarr)
+
+arr.pop()
+// console.log(arr)
+
+arr.unshift ("doing")
+// console.log(arr)
+
+arr.shift()
+
+// console.log(arr)
+
+// console.log(arr.reverse())
+// console.log(arr.length)
+
+// console.log(arr.find((d) => d ==="loyal"))
+// console.log(arr.map((d) => d ==="loyal"))
+// console.log(arr.filter((d) => d ==="loyal"))
+// console.log(typeof(arr.find((d) => d ==="loyal")))
+
+
+var alpha=["a", "g", "y", "d"]
+
+// console.log(alpha.sort((a,b) => b-a))
+// console.log(alpha.sort((a,b) => a-b))
+
+var alpha1=[1,55,74,35]
+console.log(alpha1.sort((a,b) => b-a))
+console.log(alpha1.sort((a,b) => a-b))
+
---
+++ string.js
@@ -0,0 +1,58 @@
+var username="Subash G"
+
+// console.log(username.length)
+
+var username1="the Kite is there"
+
+// console.log(username1.startsWith('he'))
+
+var email="Subashg@gamil.com"
+
+// console.log(email.toLowerCase())
+// console.log(email.toUpperCase())
+
+var firstname="ram"
+var secondname="kumar"
+// console.log(firstname.concat(secondname))
+
+var username1="the Kite is there"
+
+var data=username1.split(" ")
+// console.log(data)
+
+var data=username1.split(" ")
+
+// console.log(data.join(" | "))
+// console.log(typeof (data.join(" | ")))
+
+var username1="the Kite is there"
+
+// console.log(username1.indexOf("k"))
+
+var myusername2 =""
+
+for(i=4 ;i <= username1.length -9 ;i++){
+ myusername2 += username1[i]
+}
+
+//  console.log(myusername2)
+
+var username1="the Kite is there"
+
+// console.log(username1.slice(4,8))
+
+var email="Subashg@gamil.com"
+
+// console.log(email.split("").reverse().join(""))
+
+var username3="   god is Great    "
+console.log(username3.trim())
+
+var usernamereplace = username3.replace("god","hero")
+console.log(usernamereplace)
+console.log(usernamereplace.includes("God"))
+
+
+
+
+
